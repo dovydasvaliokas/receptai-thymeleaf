cdefb9f6f3b2be69634db16ca6d7622c4e61ac6e
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-10-18 22:54:12 +0300

    Sukurtas projekto pirminis sablonas is spring initializr

---
+++ src/main/java/dv/receptai/thymeleaf/Application.java
@@ -0,0 +1,13 @@
+package dv.receptai.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
